c499bed10d69cf6ef2bd6254f5963d537f2dccfa
caiman <me@caiman.dev>
2022-01-23 14:24:38 +0200

    [master] Flake8

--- src/strings.py
+++ src/strings.py
@@ -8,12 +8,11 @@ if __name__ == '__main__':  # pragma: no cover
     }
 
     print('%d + %d = %d' % (x, y, x + y))
-    print('%(first)d + %(second)d = %(result)d' % {'first': x, 'second': y, 'result': x+y})
+    print('%(first)d + %(second)d = %(result)d' % {'first': x, 'second': y, 'result': x + y})
     print('%(first)d + %(second)d = %(result)d' % params)
     print('{} + {} = {}'.format(x, y, x + y))
-    print('{first} + {second} = {result}'.format(first=x, second=y, result=x+y))
+    print('{first} + {second} = {result}'.format(first=x, second=y, result=x + y))
     print('{first} + {second} = {result}'.format(**params))
     print('{0} + {1} = {2}'.format(x, y, x + y))
     print('{1} + {0} = {2}'.format(x, y, x + y))
     print(f'{x} + {y} = {x+y}')
-
