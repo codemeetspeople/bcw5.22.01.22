ee6564fdfc1afc5754a4a716c0a306f9b6b32f17
caiman <me@caiman.dev>
2022-01-23 14:30:05 +0200

    [master] Type annotations

--- src/point/point.py
+++ src/point/point.py
@@ -1,43 +1,44 @@
+from __future__ import annotations
 from math import hypot
 
 
 class Point:
     __slots__ = ('_x', '_y')
 
-    def __init__(self, x=0.0, y=0.0):
+    def __init__(self, x: float = 0.0, y: float = 0.0) -> None:
         self._x = float(x)
         self._y = float(y)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f'({self.x}, {self.y})'
 
-    def __eq__(self, other):
-        if not isinstance(other, Point):
+    def __eq__(self, other: Point) -> bool:
+        if not isinstance(other, self.__class__):
             return NotImplemented
         return self.x == other.x and self.y == other.y
 
-    def __ne__(self, other):
+    def __ne__(self, other: Point) -> bool:
         return not self == other
 
-    def distance(self, other):
-        if not isinstance(other, Point):
+    def distance(self, other: Point) -> float:
+        if not isinstance(other, self.__class__):
             raise TypeError()
         return hypot(self.x - other.x, self.y - other.y)
 
     @property
-    def x(self):
+    def x(self) -> float:
         return self._x
 
     @property
-    def y(self):
+    def y(self) -> float:
         return self._y
 
     @x.setter
-    def x(self, value):
+    def x(self, value: float) -> None:
         self._x = float(value)
 
     @y.setter
-    def y(self, value):
+    def y(self, value: float) -> None:
         self._y = float(value)
 
 
